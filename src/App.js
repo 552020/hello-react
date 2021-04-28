a83cb68d5e27ff519175a3122b070078d7c23598
552020 <stefanolombardo@posteo.de>
2021-04-28 09:01:52 +0200

    add random link among a choice of links to the learn react link

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const styles = {
+  //background: "#f5f7b2",
+  background: "whitesmoke",
+
+  color: "black",
+  padding: "1vw",
+  borderRadius: "1vw", // border-radius: 1vw;
+  fontFamily: "Inconsolata",
+};
+
+const learnReact = [
+  "https://reactjs.org",
+  "https://www.codecademy.com/learn/react-101",
+  "https://www.google.com/",
+];
+
+const RandomArrInd = Math.floor(Math.random() * learnReact.length);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 style={{ color: "red", background: "orange" }}>Hello React!</h1>
-
-        {/* <div
-          style={{
-            width: "auto",
-            height: "auto",
-            padding: "1vw",
-            background: "orange",
-          }}
-        >
-          <p style={{ color: "red", background: "orange" }}>
-            This is (partially) styled inline like this:
-          </p>
-
-          <p style={{ color: "red", background: "orange" }}>
-            <code>{`
-        <h1 style={{ color: "red", background: "orange" }}>Hello React!</h1>
-          `}</code>
-          </p>
-        </div> */}
+        <p>Hello {RandomArrInd}</p>
 
         <a
+          style={styles}
           className="App-link"
-          href="https://reactjs.org"
+          href={learnReact[RandomArrInd]}
           target="_blank"
           rel="noopener noreferrer"
         >
