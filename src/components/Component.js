c1e2f628d2b12db7d04f0c3a937e5d778b391be1
552020 <stefanolombardo@posteo.de>
2021-05-03 17:41:26 +0200

    buttons do work

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,13 +1,21 @@
+import React, { useState } from "react";
 import "./Component.css";
 
 function Component(props) {
+  const [button, setButton] = useState(props.button);
+
+  const clickHandler = () => {
+    setButton("Yes! 👌");
+  };
   return (
     <div className="container">
       <div className="flex">
         <p className="p-c">{props.line}</p>
         <p className="p-c">I like {props.like}</p>
 
-        <button className="myButton">{props.button}</button>
+        <button onClick={clickHandler} className="myButton">
+          {button}
+        </button>
       </div>
     </div>
   );
