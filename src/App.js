0e4df10970dddc61f07e08b500331509f6c87367
552020 <stefanolombardo@posteo.de>
2021-05-03 16:31:19 +0200

    three new components well one component and three instances of it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Component from "./components/Component";
 
 const styles = {
   //background: "#f5f7b2",
@@ -11,38 +13,56 @@ const styles = {
   fontFamily: "Inconsolata",
 };
 
-const learnReact = [
-  "https://reactjs.org",
-  "https://www.codecademy.com/learn/react-101",
-  "https://www.google.com/",
-];
+// const clickHandler = () => {
+//   RandomArrInd();
+// };
 
-const RandomArrInd = Math.floor(Math.random() * learnReact.length);
-const myFunction = () => {
-  RandomArrInd();
-};
+// props.randomLink = learnReact[RandomArrInd];
+// let randomLink = learnReact[RandomArrInd];
 
-const myLink = learnReact[RandomArrInd];
+function App(props) {
+  // Learn React Link
 
-function App() {
+  const learnReact = [
+    "https://reactjs.org",
+    "https://www.codecademy.com/learn/react-101",
+    "https://www.google.com/",
+  ];
+  const RandomArrInd = Math.floor(Math.random() * learnReact.length);
+  const [randomLink, setLink] = useState(props.randomLink);
+
+  const clickHandler = () => {
+    setLink(learnReact[RandomArrInd]);
+  };
+
+  // Button
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 style={{ color: "red", background: "orange" }}>Hello React!</h1>
-        <p>Hello {RandomArrInd}</p>
+        {/* <p>Hello {RandomArrInd}</p> */}
 
         <a
           style={styles}
           className="App-link"
-          onclick={myFunction}
-          href={myLink}
+          onclick={clickHandler}
+          href={randomLink}
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        {/* <a href="#" class="myButton">
+          white
+        </a> */}
       </header>
+      <main>
+        <Component />
+        <Component />
+        <Component />
+      </main>
     </div>
   );
 }
---
+++ src/components/Component.js
@@ -0,0 +1,14 @@
+import "./Component.css";
+
+function Component() {
+  return (
+    <div className="container">
+      <div className="flex">
+        <p className="p-c">I'm a component!</p>
+        <button className="myButton">Click me!</button>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
