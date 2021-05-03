ccd0d2352da9709d8889ec19b09e1a8339aa7af2
552020 <stefanolombardo@posteo.de>
2021-05-03 17:14:42 +0200

    all three props has been passed yay

--- src/components/Component.js
+++ src/components/Component.js
@@ -5,7 +5,9 @@ function Component(props) {
     <div className="container">
       <div className="flex">
         <p className="p-c">{props.line}</p>
-        <button className="myButton">Click me!</button>
+        <p className="p-c">I like {props.like}</p>
+
+        <button className="myButton">{props.button}</button>
       </div>
     </div>
   );
